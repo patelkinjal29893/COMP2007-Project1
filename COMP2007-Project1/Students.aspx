752c1dbb7fdc29980925243af11c64df6b90c971
Kinjal Patel <patelkinjal298@hotmail.com>
2016-06-08 17:09:08 -0400

    Add project files.

---
+++ COMP2007-Project1/Students.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Title="Students" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Students.aspx.cs" Inherits="COMP2007_Project1.Students" %>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-offset-2 col-md-8">
+                <h1>Students List</h1>
+                <a href="StudentDetails.aspx" class="btn btn-success btn-sm"><i class="fa fa-plus"></i> Add Student</a>
+                <asp:GridView runat="server" ID="StudentsGridView" AutoGenerateColumns="false"
+                    CssClass="table table-striped table-bordered table-hover">
+                    <Columns>
+                        <asp:BoundField DataField="StudentID" HeaderText="Student ID" Visible="true" />
+                        <asp:BoundField DataField="LastName" HeaderText="Last Name" Visible="true" />
+                        <asp:BoundField DataField="FirstMidName" HeaderText="First Name" Visible="true" />
+                        <asp:BoundField DataField="EnrollmentDate" HeaderText="Enrollment Date" Visible="true"
+                            DataFormatString="{0:MMM dd, yyyy}" />
+
+                    </Columns>
+                </asp:GridView>
+
+            </div>
+        </div>
+    </div>
+
+
+</asp:Content>
---
+++ COMP2007-Project1/User_Controls/Navbar.ascx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+/**
+ * @author: Tom Tsiliopoulos
+ * @date: May 26, 2016
+ * @version: 0.0.1 - added SetActivePage method
+ */
+
+namespace COMP2007_Project1
+{
+    public partial class Navbar : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SetActivePage();
+        }
+
+        /**
+         * This method adds a css class of "active" to list items
+         * relating to the current page
+         * 
+         * @private
+         * @method SetActivePage
+         * @return {void}
+         */
+        private void SetActivePage()
+        {
+            switch (Page.Title)
+            {
+                case "Home Page":
+                    home.Attributes.Add("class", "active");
+                    break;
+                case "Students":
+                    students.Attributes.Add("class", "active");
+                    break;
+                case "Courses":
+                    courses.Attributes.Add("class", "active");
+                    break;
+                case "Departments":
+                    departments.Attributes.Add("class", "active");
+                    break;
+                case "Contact":
+                    contact.Attributes.Add("class", "active");
+                    break;
+            }
+        }
+    }
+}
